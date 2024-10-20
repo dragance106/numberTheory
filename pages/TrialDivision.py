1a180e60096f40602694480c357b553670d2d482
Dragan Stevanovic <dragance106@yahoo.com>
2024-10-20 16:35:03 +0300

    Streamlit, trial division

--- pages/TrialDivision.py
+++ pages/TrialDivision.py
@@ -31,7 +31,7 @@ st.markdown(
     whether it is prime by dividing *n* with all integers not exceeding its square root.
     """)
 
-st.text_input("Input the number *n*", key='n')
+st.number_input("Input the number *n*", key='n', value=2)
 n = int(st.session_state.n)
 if trial_division(n):
     st.write('* PRIME!')
