386e2b96598db8d3483d012db4fd29e901130d21
Dragan Stevanovic <dragance106@yahoo.com>
2024-10-21 23:11:00 +0300

    Streamlit, Euclid's algorithm

--- pages/3 Euclid's algorithm.py
+++ pages/3 Euclid's algorithm.py
@@ -3,8 +3,10 @@ import streamlit as st
 
 def gcd(a, b):
     if b == 0:
+        st.write(f'{a}')
         return a
     else:
+        st.write(f'gcd({a}, {b}) =')
         return gcd(b, a % b)
 
 
@@ -16,3 +18,12 @@ st.markdown(
     by recursively using the relation gcd(*a*, *b*) = gcd(*b*, *a* mod *b*).
     Recursion stops with gcd(*a*, 0) = *a*.
     """)
+
+st.number_input("Input the number *a*", key='a', value=30)
+st.number_input("Input the number *b*", key='b', value=24)
+
+ao = int(st.session_state.a)
+bo = int(st.session_state.b)
+
+d = gcd(ao, bo)
+
