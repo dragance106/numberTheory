28175b3bbaa30eaffa4a1411333ba9c7d0737440
Dragan Stevanovic <dragance106@yahoo.com>
2024-10-21 20:32:57 +0300

    Streamlit, trial division

--- pages/TrialDivision.py
+++ pages/TrialDivision.py
@@ -2,6 +2,18 @@ import streamlit as st
 import math
 
 
+def trial_division(n):
+    up_to = int(math.floor(math.sqrt(n)))
+    for p in range(2, up_to+1):
+        if n % p == 0:      # n is divisible by p without a remainder
+            st.write(f'{n} is divisible by {p}')
+            return False
+        else:
+            st.write(f'{n} is not divisible by {p}')
+
+    return True
+
+
 st.sidebar.markdown("Trial division")
 st.write("# Trial division")
 st.markdown(
@@ -18,22 +30,3 @@ else:
     st.write('* NOT PRIME!')
 
 
-def trial_division(n):
-    """
-    Given a positive integer n, the method determines whether it is prime
-    using trial division of n by all integers not exceeding its square root.
-
-    :param n: The integer to be tested whether it is prime.
-    :return:  True if n is prime, and False if n is not prime.
-              In such case, the method also returns the smallest divisor found.
-    """
-
-    up_to = int(math.floor(math.sqrt(n)))
-    for p in range(2, up_to+1):
-        if n % p == 0:     # n is divisible by p without a remainder
-            st.write(f'{n} is divisible by {p}')
-            return False
-        else:
-            st.write(f'{n} is not divisible by {p}')
-
-    return True
