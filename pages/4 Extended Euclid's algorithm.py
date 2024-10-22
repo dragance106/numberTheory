75319a806e8dcf0a2dc215222ee0007cbff7f297
Dragan Stevanovic <dragance106@yahoo.com>
2024-10-22 18:18:54 +0300

    Streamlit, Extended Euclid's algorithm

--- pages/4 Extended Euclid's algorithm.py
+++ pages/4 Extended Euclid's algorithm.py
@@ -1,38 +1,17 @@
 import streamlit as st
 
 
-def extended_gcd(a, b, cols):
+def extended_gcd(a, b, lines):
     if b == 0:
-        # st.markdown(f'| {a} | 0 |   | {a} | 1 | 0 |')
-        cols[0].write(a)
-        cols[1].write('0')
-        cols[2].write(' ')
-        cols[3].write(a)
-        cols[4].write('1')
-        cols[5].write('0')
-
+        lines.append(f'| {a} | 0 |   | {a} | 1 | 0 |')
         return a, 1, 0
     else:
-        # st.markdown(f'| {a} | {b} | {a//b} |  |  |  |')
-        cols[0].write(a)
-        cols[1].write(b)
-        cols[2].write(a//b)
-        cols[3].write(' ')
-        cols[4].write(' ')
-        cols[5].write(' ')
-
-        (d, x1, y1) = extended_gcd(b, a % b, cols)
+        lines.append(f'| {a} | {b} | {a//b} |  |  |  |')
+        (d, x1, y1) = extended_gcd(b, a % b, lines)
 
         x = y1
         y = x1 - (a//b)*y1
-
-        # st.markdown(f'| {a} | {b} | {a//b} | {d} | {x} | {y} |')
-        cols[0].write(a)
-        cols[1].write(b)
-        cols[2].write(a//b)
-        cols[3].write(d)
-        cols[4].write(x)
-        cols[5].write(y)
+        lines.append(f'| {a} | {b} | {a//b} | {d} | {x} | {y} |')
 
         return d, x, y
 
@@ -71,12 +50,10 @@ st.number_input("Input the number *b*", key='b', value=24)
 ao = int(st.session_state.a)
 bo = int(st.session_state.b)
 
-colso = st.columns(6)
+lineso = ['| *a* | *b* | int(*a/b*) | *d* | *x* | *y* |',
+          '| --- | --- | ---        | --- | --- | --- |']
 
-# st.markdown("""
-# | *a* | *b* | int(*a/b*) | *d* | *x* | *y* |
-# | --- | --- | ---        | --- | --- | --- |
-# """)
+do, xo, yo = extended_gcd(ao, bo, lineso)
 
-do = extended_gcd(ao, bo, colso)
+st.markdown(lineso)
 
