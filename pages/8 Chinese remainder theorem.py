c421d2dc44a3a822596295931e5218a681cc4aa4
Dragan Stevanovic <dragance106@yahoo.com>
2024-10-30 13:06:28 +0300

    Chinese remainder theorem

--- pages/8 Chinese remainder theorem.py
+++ pages/8 Chinese remainder theorem.py
@@ -15,17 +15,17 @@ st.markdown(
 
     *x* = *a*k (mod *n*k)
 
-    provided that the moduli *n*1, *n*2, ..., *n*k are pairwise relatively prime, and
-    where the solution is wanted modulo *n*=*n*1&centerdot;&*n*2&centerdot;...&centerdot;*n*k.
+    provided the moduli *n*1, *n*2, ..., *n*k are pairwise relatively prime, and
+    that the solution is wanted modulo *n*=*n*1&centerdot;*n*2&centerdot;...&centerdot;*n*k.
     
     The main point is to determine the numbers *c*1, *c*2, ..., *c*k such that
-    for *c_i* we have 
+    for *ci* we have 
     
-    *c_i* = 1 (mod *n_i*)
+    *ci* = 1 (mod *ni*)
     
     and 
     
-    *c_i* = 0 (mod *n_j*)
+    *ci* = 0 (mod *nj*)
     
     for any *j* different from *i*. 
     """)
