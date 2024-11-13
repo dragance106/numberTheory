de0ab393748c6451ac787eb139dc466effe33647
Dragan Stevanovic <dragance106@yahoo.com>
2024-11-13 20:21:30 +0300

    Chinese remainder theorem

--- pages/8 Euler's phi function.py
+++ pages/8 Euler's phi function.py
@@ -9,20 +9,14 @@ st.markdown(
     that are relatively prime to $n$.
     
     This function can be computed according to the formula
-    $$
-    \phi(n) = n \prod_{\substack{p\mbox{ is prime} \\ p\div n}} \left(1-\frac1p\right).
-    $$
+    $$\phi(n) = n \prod_{\substack{p\mbox{ is prime} \\ p\div n}} \left(1-\frac1p\right).$$
     
     It is multiplicative, 
     meaning that $\phi(mn)=\phi(m)\phi(n)$ for relatively prime $m$ and $n$.
     Also if we know the prime decomposition
-    $$
-    n = p_1^{\alpha_1} p_2^{\alpha_2} \cdot\dots\cdot p_k^{\alpha_k},
-    $$
+    $$n = p_1^{\alpha_1} p_2^{\alpha_2} \cdot\dots\cdot p_k^{\alpha_k},$$
     then
-    $$
-    \phi(n) = p_1^{\alpha_1-1}(p_1-1) p_2^{\alpha_2-1}(p_2-1) \cdot\dots\cdot p_k^{\alpha_k-1}(p_k-1).
-    $$
+    $$\phi(n) = p_1^{\alpha_1-1}(p_1-1) p_2^{\alpha_2-1}(p_2-1) \cdot\dots\cdot p_k^{\alpha_k-1}(p_k-1).$$
     """)
 
 st.number_input("Input the number $n$", key='n', value=10)
