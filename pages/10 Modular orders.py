5b64e9e43ed7d1958c9707c1bdd703f5c549e401
Dragan Stevanovic <dragance106@yahoo.com>
2024-11-20 15:00:23 +0300

    Modular orders

--- pages/10 Modular orders.py
+++ pages/10 Modular orders.py
@@ -24,7 +24,7 @@ def print_orders(sto, n):
             while b != 1:
                 k = k+1
                 b = (b*a) % n
-                lines = lines + f'\n{b} | '
+                lines = lines + f' {b} | '
 
             # empty cells from the order to phi(n)
             lines = lines + "--- | "*(fi-k)
