f824c0691abd5d659db0b4c5e39d67be277a3cff
Dragan Stevanovic <dragance106@yahoo.com>
2025-10-06 14:25:57 +0300

    Buttons for Android

--- pages/1 Trial Division.py
+++ pages/1 Trial Division.py
@@ -24,11 +24,10 @@ st.markdown(
 
 st.number_input("Input the number *n*", key='n', value=37)
 n = int(st.session_state.n)
-st.button("Do it", on_click=trial_division, args=[n])
-
 if trial_division(n):
     st.write('* PRIME!')
 else:
     st.write('* NOT PRIME!')
 
+st.button("Do it", on_click=trial_division, args=[n])
 
